00b773e694acaa9d970b2a1d0c87bb336046fa6b
Omkar Desai <omkar.desai.1991@gmail.com>
2026-04-05 15:24:52 -0700

    :zap: Add new features and optimize

--- .oh-my-posh/PowerShell/Microsoft.PowerShell_profile.ps1
+++ .oh-my-posh/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 oh-my-posh init pwsh --config ~/.oh-my-posh/themes/powerlevel10k_lean.omp.json | Invoke-Expression
 
+Import-Module -Name Terminal-Icons
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
